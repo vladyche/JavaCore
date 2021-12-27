fd12c7acae8275d42581f8c508b0682d5a221a84
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-12-28 01:54:20 +0200

    write to file

--- src/io/writefile/Main.java
+++ src/io/writefile/Main.java
@@ -1,7 +1,21 @@
 package io.writefile;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
+        String[] strings = {"string-1", "string-2", "string-3"};
 
+        String pathToFile = "src/io/writefile/file/file";
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathToFile))){
+            for(String s : strings){
+                bufferedWriter.write(s);
+                bufferedWriter.write(System.lineSeparator());
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
