ee4ca4e6f3f7e3e79a03e5bc73159073e43a62cb
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-12-29 01:37:22 +0200

    linked list demo

--- src/collections/list/linkedlist/Main.java
+++ src/collections/list/linkedlist/Main.java
@@ -1,7 +1,28 @@
 package collections.list.linkedlist;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+        List<String> list = new LinkedList<>();
+        list.add("string-1");
+        list.add("string-2");
+        list.add("string-2");
 
+        for(String s : list){
+            System.out.println(s);
+        }
+
+        //add to position
+        list.add(3, "string-new");
+
+        //size
+        System.out.println(list.size());
+
+        //get by index
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
