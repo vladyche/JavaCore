471be55d7d3e32533e21c2e7a3fa162784b8f9a5
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-12-29 02:03:46 +0200

    array list demo

--- src/collections/list/arraylist/Main.java
+++ src/collections/list/arraylist/Main.java
@@ -1,7 +1,17 @@
 package collections.list.arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+        List<String> strings = new ArrayList<>();
+        strings.add("string-1");
+        strings.add("string-2");
+        strings.add("string-3");
 
+        for(String string : strings){
+            System.out.println(string);
+        }
     }
 }
