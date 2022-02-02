4bdf04db6239f6e7dab4e253130b0d54a9f6bffa
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-02-02 23:19:37 +0200

    Primitive Data Types

---
+++ src/base/datatypes/primitive/Main.java
@@ -0,0 +1,39 @@
+package base.datatypes.primitive;
+
+public class Main {
+    public static void main(String[] args){
+
+        //byte
+        byte byteVariable = 0;
+
+        //short
+        short shortVariable = 0;
+
+        //int
+        int intVariable = 0;
+
+        //long
+        long longVariable = 0;
+
+        //float
+        float floatVariable = 0;
+
+        //double
+        double doubleVariable = 0;
+
+        //boolean
+        boolean booleanVariable = true;
+
+        //char
+        char charVariable = 'a';
+
+        System.out.println("Byte variable: " + byteVariable);
+        System.out.println("Short variable: " + shortVariable);
+        System.out.println("Int variable: " + intVariable);
+        System.out.println("Long variable: " + longVariable);
+        System.out.println("Float variable: " + floatVariable);
+        System.out.println("Double variable: " + doubleVariable);
+        System.out.println("Boolean variable: " + booleanVariable);
+        System.out.println("Char variable: " + charVariable);
+    }
+}
