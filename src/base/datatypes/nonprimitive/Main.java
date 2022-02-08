c94f33f7925d157abd63f4d663999cbdea4d26a6
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-02-08 02:47:31 +0200

    String data type

---
+++ src/base/datatypes/nonprimitive/Main.java
@@ -0,0 +1,13 @@
+package base.datatypes.nonprimitive;
+
+public class Main {
+    public static void main(String[] args){
+        String s1 = "String";
+        String s2 = new String("New String");
+
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+
+        System.out.println(s2);
+    }
+}
