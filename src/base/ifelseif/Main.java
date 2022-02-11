62022343d011c645fed6694946ccd119efcf0bbf
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-02-11 21:37:30 +0200

    exploring methods of class Object

--- src/base/ifelseif/Main.java
+++ src/base/ifelseif/Main.java
@@ -3,5 +3,17 @@ package base.ifelseif;
 public class Main {
     public static void main(String[] args){
 
+//        if(){
+//
+//        }else if(){
+//
+//        }else if(){
+//
+//        }else if(){
+//
+//        }else{
+//
+//        }
+
     }
 }
---
+++ src/oop/objectclass/Main.java
@@ -0,0 +1,22 @@
+package oop.objectclass;
+
+public class Main {
+    public static void main(String[] args){
+        Object o = new Object();
+
+        //o.wait();
+        //o.notify();
+        //o.notifyAll();
+
+        //o.equals()
+        //o.hashCode()
+        //o.toString()
+
+        //o.getClass()
+    }
+
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+}
