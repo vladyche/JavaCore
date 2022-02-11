0d35d9e3016b62f59ad492b0a8e87dac79a49592
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-02-11 22:06:52 +0200

    switch case example

--- src/base/ifelse/Main.java
+++ src/base/ifelse/Main.java
@@ -2,7 +2,6 @@ package base.ifelse;
 
 public class Main {
     public static void main(String[] args){
-
         for (int i = 0; i < 10; i++) {
             if((i % 2) == 0){
                 System.out.println("even number: " + i);
@@ -10,6 +9,5 @@ public class Main {
                 System.out.println("odd number: " + i);
             }
         }
-
     }
 }
--- src/base/switchcase/Main.java
+++ src/base/switchcase/Main.java
@@ -3,5 +3,72 @@ package base.switchcase;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 20; i++) {
+            switch(i){
+                case 1: {
+                    System.out.println(i + " month is January");
+                    break;
+                }
+                case 2: {
+                    System.out.println(i + " month is February");
+                    break;
+                }
+
+                case 3: {
+                    System.out.println(i + " month is March");
+                    break;
+                }
+
+                case 4: {
+                    System.out.println(i + " month is April");
+                    break;
+                }
+
+                case 5: {
+                    System.out.println(i + " month is May");
+                    break;
+                }
+
+                case 6: {
+                    System.out.println(i + " month is June");
+                    break;
+                }
+
+                case 7: {
+                    System.out.println(i + " month is July");
+                    break;
+                }
+
+                case 8: {
+                    System.out.println(i + " month is August");
+                    break;
+                }
+
+                case 9: {
+                    System.out.println(i + " month is September");
+                    break;
+                }
+
+                case 10: {
+                    System.out.println(i + " month is October");
+                    break;
+                }
+
+                case 11: {
+                    System.out.println(i + " month is November");
+                    break;
+                }
+
+                case 12: {
+                    System.out.println(i + " month is December");
+                    break;
+                }
+
+                default: {
+                    System.out.println(i + " is not of month number");
+                    break;
+                }
+            }
+        }
     }
 }
